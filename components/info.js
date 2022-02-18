621f0ef267435a3bd676f5dcb0ae85754b80e48f
bartosz-skejcik <bartek@paczesny.pl>
2022-02-18 19:39:16 +0100

    asdasd

--- components/info.js
+++ components/info.js
@@ -23,7 +23,7 @@ function Info() {
             <p className='cursor-pointer mb-1'>Współpracujemy z najlepszymi projektantami aby jakość i rzadkość peleryny pozostaje wysoki!</p>
             <div className="cursor-pointer relative w-36 h-2 bg-green-500 rounded mt-3"></div>
         </div>
-        <div className="cursor-pointer flex flex-col items-center justify-center text-center w-60 h-60 rounded-3xl bg-gray-700">
+        <div className="cursor-pointer flex flex-col items-center justify-center text-center w-60 h-60 rounded-3xl bg-gray-700 mt-10 md:lg-0">
             <PencilIcon className="cursor-pointer block h-12 w-12 mb-1" aria-hidden="true" />
             <h1 className='cursor-pointer text-2xl font-semibold mb-3'>Custom Cape</h1>
             <p className='cursor-pointer mb-1'>Chcesz mieć możliwość zaprojektowania własnej peleryny? Na naszym discordzie możesz podesłać swój design</p>
